29ebdb08911fe86a52051ca10ed5b91cf4b351eb
Dmitry Varin <varindv@gmail.com>
2020-11-01 02:35:32 +0300

    fix headers

--- project/src/main.c
+++ project/src/main.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
 
 #include "parce.h"
 

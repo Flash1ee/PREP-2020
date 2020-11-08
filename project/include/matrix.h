2b8081c0121c0e8496eca67ce3c38654b61475b2
Dmitry Varin <varindv@gmail.com>
2020-11-08 14:44:45 +0300

    make hw-5

--- project/include/matrix.h
+++ project/include/matrix.h
@@ -1,41 +1,50 @@
 #pragma once
 
-#include <vector>
+#include <cmath>
+#include <iomanip>
 #include <istream>
+#include <limits>
+#include <ostream>
+#include <vector>
 
 namespace prep {
 class Matrix {
+ private:
+    size_t m_rows;
+    size_t m_cols;
+    std::vector<std::vector<double> > m_data;
+
  public:
-  explicit Matrix(size_t rows = 0, size_t cols = 0);
-  explicit Matrix(std::istream& is);
-  Matrix(const Matrix& rhs) = default;
-  Matrix& operator=(const Matrix& rhs) = default;
-  ~Matrix() = default;
+    explicit Matrix(size_t rows = 0, size_t cols = 0);
+    explicit Matrix(std::istream& is);
+    Matrix(const Matrix& rhs) = default;
+    Matrix& operator=(const Matrix& rhs) = default;
+    ~Matrix() = default;
 
-  size_t getRows() const;
-  size_t getCols() const;
+    size_t getRows() const;
+    size_t getCols() const;
 
-  double operator()(size_t i, size_t j) const;
-  double& operator()(size_t i, size_t j);
+    double operator()(size_t i, size_t j) const;
+    double& operator()(size_t i, size_t j);
 
-  bool operator==(const Matrix& rhs) const;
-  bool operator!=(const Matrix& rhs) const;
+    bool operator==(const Matrix& rhs) const;
+    bool operator!=(const Matrix& rhs) const;
 
-  Matrix operator+(const Matrix& rhs) const;
-  Matrix operator-(const Matrix& rhs) const;
-  Matrix operator*(const Matrix& rhs) const;
+    Matrix operator+(const Matrix& rhs) const;
+    Matrix operator-(const Matrix& rhs) const;
+    Matrix operator*(const Matrix& rhs) const;
 
-  Matrix operator*(double val) const;
+    Matrix operator*(double val) const;
 
-  friend
-  Matrix operator*(double val, const Matrix& matrix);
-  friend
-  std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+    friend Matrix operator*(double val, const Matrix& matrix);
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
 
-  Matrix transp() const;
-  double det() const;
-  Matrix adj() const;
-  Matrix inv() const;
+    friend void get_minor(const Matrix& src, Matrix& tmp, size_t cur_col, size_t cur_row);
+
+    Matrix transp() const;
+    double det() const;
+    Matrix adj() const;
+    Matrix inv() const;
 };
 
 Matrix operator*(double val, const Matrix& matrix);
